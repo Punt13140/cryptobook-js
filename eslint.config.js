388565a7bc60dfa82399be0ef4281f8af427c132
NICOLAU Bastien <bastien.nicolau@univ-amu.fr>
2024-06-12 14:39:37 +0200

    Setup Eslint
    Install and setup eslint to work with Prettier

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import { fixupConfigRules } from '@eslint/compat';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.jsx'],
+    languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } },
+  },
+  ...fixupConfigRules(pluginReactConfig),
+  eslintConfigPrettier,
+  {
+    ignores: [
+      'coverage',
+      'public',
+      'dist',
+      'pnpm-lock.yaml',
+      'pnpm-workspace.yaml',
+    ],
+  },
+];
